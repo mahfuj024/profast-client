241d013fc53d0a4634156cbd208f8cf035888c68
Mahfuj Islam <mahfujislam024@gmail.com>
2025-09-13 22:54:12 +0600

    add marqueeBrand section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from './Banner/Banner'
 import Works from './Works/Works'
 import Services from './Services/Services'
+import MarqueeBrand from './MarqueeBrand/MarqueeBrand'
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Banner></Banner>
       <Works></Works>
       <Services></Services>
+      <MarqueeBrand></MarqueeBrand>
     </div>
   )
 }
---
+++ src/pages/Home/MarqueeBrand/MarqueeBrand.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Marquee from "react-fast-marquee";
+import amazon from "../../../assets/brands/amazon.png"
+import casio from "../../../assets/brands/casio.png"
+import moonstar from "../../../assets/brands/moonstar.png"
+import randstad from "../../../assets/brands/randstad.png"
+import start from "../../../assets/brands/start.png"
+
+function MarqueeBrand() {
+    return (
+        <div className='mt-8 md:mt-12 lg:mt-20 p-1 lg:p-0'>
+            <h1 className='text-center text-base-300 text-2xl md:text-3xl font-extrabold'>We've helped thousands of sales teams</h1>
+
+            <div className='mt-6 md:mt-8 lg:mt-10'>
+                <Marquee>
+                    <div className='flex items-center gap-7 md:gap-18 lg:gap-60'>
+                        <img src={amazon} alt="amazon logo" className='ml-4 lg:ml-0'/>
+                        <img src={casio} alt="casio logo" />
+                        <img src={moonstar} alt="moonstar logo" />
+                        <img src={start} alt="start logo" />
+                    </div>
+
+                </Marquee>
+            </div>
+        </div>
+    )
+}
+
+export default MarqueeBrand
