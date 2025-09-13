8c6b6b3375dfa8b854e146ed8306ad80e81c4453
Mahfuj Islam <mahfujislam024@gmail.com>
2025-09-13 19:04:22 +0600

    add banner section

--- src/pages/Home/Works/Works.jsx
+++ src/pages/Home/Works/Works.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
-
+import bookingIcon from "../../../assets/bookingIcon.png"
 
 function Works() {
     return (
-        <div className="mt-6 md:mt-10 lg:mt-15 p-1 lg:p-0">
-            <h1 className='font-bold lg:font-extrabold text-2xl lg:text-3xl text-base-300'>How it Works</h1>
+        <div className="mt-6 md:mt-10 lg:mt-15 lg:p-0 max-w-[1282px] mx-auto">
+            <h1 className='font-bold lg:font-extrabold text-3xl text-base-300 ml-15 md:ml-8 lg:ml-0'>How it Works</h1>
 
-            <div className='mt-4 md:mt-6 lg:mt-8'>
+            <div className='mt-4 md:mt-6 lg:mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center gap-6'>
 
-                <div>
-                    <img src="" alt="" />
-                    <h2>Booking Pick & Drop</h2>
-                    <p>From personal packages to business shipments — we deliver on time, every time.</p>
+                <div className='p-8 rounded-2xl bg-white max-w-[302px]'>
+                    <img src={bookingIcon} alt="" />
+                    <h2 className='text-base-300 font-bold text-[20px] mt-6'>Booking Pick & Drop</h2>
+                    <p className='text-base-200 text-base mt-4'>From personal packages to business shipments — we deliver on time, every time.</p>
                 </div>
+
+                <div className='p-8 rounded-2xl bg-white max-w-[302px]'>
+                    <img src={bookingIcon} alt="" />
+                    <h2 className='text-base-300 font-bold text-[20px] mt-6'>Booking Pick & Drop</h2>
+                    <p className='text-base-200 text-base mt-4'>From personal packages to business shipments — we deliver on time, every time.</p>
+                </div>
+
+
+                <div className='p-8 rounded-2xl bg-white max-w-[302px]'>
+                    <img src={bookingIcon} alt="" />
+                    <h2 className='text-base-300 font-bold text-[20px] mt-6'>Booking Pick & Drop</h2>
+                    <p className='text-base-200 text-base mt-4'>From personal packages to business shipments — we deliver on time, every time.</p>
+                </div>
+
+
+                <div className='p-8 rounded-2xl bg-white max-w-[302px]'>
+                    <img src={bookingIcon} alt="" />
+                    <h2 className='text-base-300 font-bold text-[20px] mt-6'>Booking Pick & Drop</h2>
+                    <p className='text-base-200 text-base mt-4'>From personal packages to business shipments — we deliver on time, every time.</p>
+                </div>
+
 
 
             </div>
